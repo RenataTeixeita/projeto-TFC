d0f2cc9fb09ce55f13a60b88defb0e026245b577
Renata <renatateixeira.est@gmail.com>
2022-03-29 15:19:47 -0300

    Atualizando o packages.npm

--- app/backend/src/controllers/userController.ts
+++ app/backend/src/controllers/userController.ts
@@ -3,7 +3,6 @@ import UserService from '../services/UserService';
 
 const login = async (req: Request, res: Response) => {
   const { email, password } = req.body;
-  // console.log(req.body);
   const makeLogin = await UserService.login(email, password);
   if (typeof (makeLogin) === 'string') {
     return res.status(401).json({ message: makeLogin });
--- app/backend/src/middlewares/schemaLogin.ts
+++ app/backend/src/middlewares/schemaLogin.ts
@@ -1,11 +1,14 @@
 import Joi = require('joi');
 
+const message = 'All fields must be filled';
+
 const schemaLogin = Joi.object({
   email: Joi.string().email().required().messages({
-    'any.required': 'All fields must be filled',
+    'any.required': message,
+    'string.email': message,
   }),
   passwordInput: Joi.string().min(6).required().messages({
-    'any.required': 'All fields must be filled',
+    'any.required': message,
   }),
 });
 
