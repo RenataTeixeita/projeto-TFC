d9a0d2cb69cad6db029404c501c5b0ba5cec0cc5
Renata <renatateixeira.est@gmail.com>
2022-04-04 10:21:19 -0300

    Criei os testes do método post da rota login

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -1,9 +1,11 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
-import chaiHttp = require('chai-http');
+// @ts-ignore
+import chaiHttp from 'chai-http';
 
 import { app } from '../app';
-import Example from '../database/models/ExampleModel';
+// import Example from '../database/models/ExampleModel';
+import Users from '../database/models/Users';
 
 import { Response } from 'superagent';
 
@@ -11,34 +13,34 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Seu teste', () => {
+describe('Verifica a rota Login', () => {
   /**
    * Exemplo do uso de stubs com tipos
    */
 
-  // let chaiHttpResponse: Response;
+  let chaiHttpResponse: Response;
 
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
-
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
-
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
-
-  //   expect(...)
-  // });
-
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
+  before(async () => {
+    sinon
+      .stub(Users, "findOne")
+      .resolves({
+          // ...<Seu mock>
+          where: { email: 'admin@admin.com' }
+        } as unknown as Users);
   });
+
+  after(()=>{
+    (Users.findOne as sinon.SinonStub).restore();
+  })
+
+  it('Verifica o método POST', async () => {
+    chaiHttpResponse = await chai
+       .request(app).post('/login')
+       .send({ email: 'admin@admin.com', password: 'secret_admin' });
+    expect(chaiHttpResponse.status).to.be.equal(200);
+  });
+
+  // it('Seu sub-teste', () => {
+  //   expect(false).to.be.eq(true);
+  // });
 });
