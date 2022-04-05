47ddccc1c536be2d0f7299332169ae3bc6aa404f
Renata <renatateixeira.est@gmail.com>
2022-04-05 14:39:28 -0300

    Requisitos 11 e 13

--- app/backend/src/middlewares/verifyLogin.ts
+++ app/backend/src/middlewares/verifyLogin.ts
@@ -1,6 +1,10 @@
 import schemaLogin from './schemaLogin';
 
 const verifyLogin = async (email: string, password: string) => {
+  if (!email || !password) {
+    console.log('oi');
+    return 'All fields must be filled';
+  }
   try {
     const { error } = schemaLogin({ email, password });
     if (error) return error.details[0].message;
