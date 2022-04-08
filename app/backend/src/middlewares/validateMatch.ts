7ef1267c82ec733d19b1ddb258aaf8bf289887ca
Renata <renatateixeira.est@gmail.com>
2022-04-08 18:25:20 -0300

    Requisito 26 - validação

--- app/backend/src/middlewares/validateMatch.ts
+++ app/backend/src/middlewares/validateMatch.ts
@@ -1,8 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
+import Clubs from '../database/models/Clubs';
 
 const validMatch = async (req: Request, res: Response, next: NextFunction) => {
   const { homeTeam, awayTeam } = req.body;
-  if (!homeTeam || !awayTeam) {
+  const isteam1 = await Clubs.findByPk(Number(homeTeam));
+  const isteam2 = await Clubs.findByPk(Number(awayTeam));
+  if (isteam1 === null || isteam2 === null) {
     return next(res.status(401).json({ message: 'There is no team with such id!' }));
   }
   if (homeTeam === awayTeam) {
