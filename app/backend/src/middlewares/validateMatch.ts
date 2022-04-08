a85a0998d9b8b561b4103f56f0bbbaccf584f8bf
Renata <renatateixeira.est@gmail.com>
2022-04-08 16:32:19 -0300

    Requisito 26

--- app/backend/src/middlewares/validateMatch.ts
+++ app/backend/src/middlewares/validateMatch.ts
@@ -2,9 +2,9 @@ import { Request, Response, NextFunction } from 'express';
 
 const validMatch = async (req: Request, res: Response, next: NextFunction) => {
   const { homeTeam, awayTeam } = req.body;
-  // if (!homeTeam || !awayTeam) {
-  //   return next(res.status(401).json({ message: 'There is no team with such id!' }));
-  // }
+  if (!homeTeam || !awayTeam) {
+    return next(res.status(401).json({ message: 'There is no team with such id!' }));
+  }
   if (homeTeam === awayTeam) {
     return next(res.status(401)
       .json({ message: 'It is not possible to create a match with two equal teams' }));
